78e82b6ba39ecd9d4f073e26aae53a5c54bba2ce
Guillermo Tala <guille86598@gmail.com>
2016-06-09 21:35:58 -0300

    test

--- PuntosInteres/src/main/java/gestores/GestorPoi.java
+++ PuntosInteres/src/main/java/gestores/GestorPoi.java
@@ -3,7 +3,13 @@ package gestores;
 import clases.*;
 
 public class GestorPoi {
+	
 	public ResultadoConsulta ObtenerPoiPorCriterio(String criterio) {
 		return new ResultadoConsulta();
+	}
+	
+	public float CalcularDistanciaEntrePuntos(POI puntoUno, POI puntoDos)
+	{
+		return puntoDos.getLocacion().getCoordenada().getLatitud();
 	}
 }

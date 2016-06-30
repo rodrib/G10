cfe100799bce8585fd596ef2ff8fd717546dee04
rodrib <rodrib@users.noreply.github.com>
2016-06-29 23:37:21 -0300

    buscar poi

--- PuntosInteres/src/main/java/utn/dds/g10/entidades/ResultadoConsulta.java
+++ PuntosInteres/src/main/java/utn/dds/g10/entidades/ResultadoConsulta.java
@@ -18,6 +18,27 @@ public class ResultadoConsulta {
 	public List<POI> getPuntos() {
 		return puntos;
 	}
+	
+	public static List<POI> buscarPOI (String tag){
+		List<POI> listaPOIsSeleccionados = new ArrayList <POI>();
+		for(int i=0;i<listaPOIs.size();i++) {
+			if (listaPOIs.get(i).buscarTag(tag)){
+				listaPOIsSeleccionados.add(listaPOIs.get(i));
+			}
+		}
+		return listaPOIsSeleccionados;
+	}
+	
+	List<String> POI = new ArrayList<String>();			
+	list.add("Banco Frances");
+	list.add("Banco ICBC");
+	list.add("Banco HSBC");
+	list.add("CGP 1");
+	list.add("CGP 2");
+	list.add("Kiosco MO");
+	list.add("Kiosco Nep");
+	list.add("Libreria AS");
+	list.add("Libreria SI");
 
 	public void setPuntos(List<POI> puntos) {
 		this.puntos = puntos;

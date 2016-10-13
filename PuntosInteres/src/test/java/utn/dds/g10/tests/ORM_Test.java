1b8319b6cca97cecbc0b8db35adeed20b380de59
Guillermo Tala <guille86598@gmail.com>
2016-10-12 23:50:24 -0300

    hibernate dependencias POM

---
+++ PuntosInteres/src/test/java/utn/dds/g10/tests/ORM_Test.java
@@ -0,0 +1,14 @@
+package utn.dds.g10.tests;
+
+import org.hibernate.Hibernate;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ORM_Test {
+
+	@Test
+	public void OrmTest() throws Exception {
+
+	}
+
+}

007c8d175ef706f40fd2dd7a49fe308421da82b3
gggarin <gggarin@hotmail.com>
2016-10-15 21:12:10 -0300

    login

---
+++ PuntosInteres/src/main/java/utn/dds/g10/beans/loginBean.java
@@ -0,0 +1,22 @@
+package utn.dds.g10.beans;
+
+public class loginBean {
+	
+	private String name;
+	private String password;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
+
+}

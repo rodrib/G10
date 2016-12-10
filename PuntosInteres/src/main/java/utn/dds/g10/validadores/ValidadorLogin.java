945f0c6062240b5489aacb5b628f05eea9cf52a1
gtala <guille86598@gmail.com>
2016-12-10 19:57:18 -0300

    Vesrsión estable con manejo de permisos por rol

--- PuntosInteres/src/main/java/utn/dds/g10/validadores/ValidadorLogin.java
+++ PuntosInteres/src/main/java/utn/dds/g10/validadores/ValidadorLogin.java
@@ -11,8 +11,6 @@ import javax.faces.validator.ValidatorException;
 import utn.dds.g10.DAO.DaoRelacional;
 import utn.dds.g10.entidades.administracion.Usuario;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 
 @FacesValidator("utn.dds.g10.validadores.ValidadorLogin")
@@ -36,8 +34,7 @@ public class ValidadorLogin implements Validator {
 			FacesMessage msg = new FacesMessage("Error de conexión.", "Ocurrió un error al intentar conectarse a la base de datos.");
 			msg.setSeverity(FacesMessage.SEVERITY_ERROR);
 			throw new ValidatorException(msg);	
-		}
-		
+		}		
 		
 		if(!usuarioIngresado.equals(usuario.getNombre()) || !pass.equals(usuario.getPassword()))
 		{
@@ -45,5 +42,6 @@ public class ValidadorLogin implements Validator {
 			msg.setSeverity(FacesMessage.SEVERITY_ERROR);
 			throw new ValidatorException(msg);	
 		}
+		
 	}
 }
